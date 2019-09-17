a795c602af5e3cbab7d051a7143827e3697fbb2b
misrori <ormraat.pte@gmail.com>
2019-09-17 13:31:55 +0200

    init

---
+++ task for the first week.R
@@ -0,0 +1,28 @@
+#tasks
+
+# set up ssh with R studio.
+# create a repo and push code from Rstudio 
+
+
+# read the cars_2019-02-22.rds
+# save it to csv
+# compare the size of the rds and csv file, what is the comprassion ratio of the rds
+
+#
+#work with the data
+#muszaki_vizsga_ervenyes column is the last day before 'technical exam'(every two year the car have to pass an exam) 
+
+#how many car has experi
+
+# how many car has 'tolatókamera'(reverse camera) in the 'muszaki' column
+# which car is the oldest(evjarat) which can run 0-100 km/h (gyorsulas_0_rol_100_km_h_ra column) below 5 seconds()
+
+#on average which brand has the cheapest cars
+
+
+# on the cars_pages you can find many files. 
+# read all the file into one file with different methods.
+  #1. use for loop
+  #2. use lapply
+  
+#write a function which needs just the path, and paste all the files (in the path) into one file and return with the file. 
